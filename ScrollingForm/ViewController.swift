7815f1213acb5569bb9e55481b6d28e5c8330781
Krishna Alex <krishna.aromal@gmail.com>
2023-04-10 22:13:13 -0700

    Created scroll view project

--- ScrollingForm/ViewController.swift
+++ ScrollingForm/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        registerForKeyboardNotifications()
+        
+    }
+    
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide),
+            name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(_ notificiation: NSNotification) {
+            guard let info = notificiation.userInfo,
+            let keyboardFrameValue = info[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+            let keyboardFrame = keyboardFrameValue.cgRectValue
+            let keyboardSize = keyboardFrame.size
+            let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardSize.height, right: 0.0)
+            scrollView.contentInset = contentInsets
+            scrollView.scrollIndicatorInsets = contentInsets
+    }
+    
+    @objc func keyboardWillHide(_ notification: NSNotification) {
+            let contentInsets = UIEdgeInsets.zero
+            scrollView.contentInset = contentInsets
+            scrollView.scrollIndicatorInsets = contentInsets
     }
 
-
+    //Touch gesture to hide keyboard when touched outside the text field.
+    @IBAction func hideKeyboard(_ sender: UITapGestureRecognizer) {
+        
+        view.endEditing(true)
+    }
+    
 }
 
